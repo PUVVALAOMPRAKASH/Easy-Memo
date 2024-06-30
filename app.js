fe678ce03c2c54fc9cd742c9368476b10d0fd03b
Puvvala Omprakash <166001286+PUVVALAOMPRAKASH@users.noreply.github.com>
2024-07-01 00:03:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,83 +3,86 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const Product = require('./models/product');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 const app = express();
 
 const mongoOptions = {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}
+};
 
 mongoose.connect(process.env.MONGO_URI, mongoOptions)
     .then(() => {
         app.listen(3000, () => {
-            console.log("APP IS LISTENING ON PORT 3000!")
+            console.log("APP IS LISTENING ON PORT 3000!");
         });
     })
     .catch(err => {
-        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log("OH NO MONGO CONNECTION ERROR!!!!");
         console.log(err);
     });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'))
-app.use(express.static(__dirname + "/public"))
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + "/public"));
 
+// Update routes from /products to /memo
 app.get('/', async (req, res) => {
     const { title } = req.query;
     if (title) {
-        const products = await Product.find({ title })
-        res.render('products/index', { products, title })
+        const products = await Product.find({ title });
+        res.render('products/index', { products, title });
     } else {
-        const products = await Product.find({})
-        res.render('products/index', { products })
+        const products = await Product.find({});
+        res.render('products/index', { products });
     }
 });
 
-app.get('/products', async (req, res) => {
+app.get('/memo', async (req, res) => {
     const { title } = req.query;
     if (title) {
-        const products = await Product.find({ title })
-        res.render('products/index', { products, title })
+        const products = await Product.find({ title });
+        res.render('products/index', { products, title });
     } else {
-        const products = await Product.find({})
-        res.render('products/index', { products })
+        const products = await Product.find({});
+        res.render('products/index', { products });
     }
 });
 
-app.get('/products/new', (req, res) => {
+app.get('/memo/new', (req, res) => {
     res.render('products/new');
 });
 
-app.post('/products', async (req, res) => {
+app.post('/memo', async (req, res) => {
     const newProduct = new Product(req.body);
     await newProduct.save();
-    res.redirect(`/products/${newProduct._id}`)
+    res.redirect(`/memo/${newProduct._id}`);
 });
 
-app.get('/products/:id', async (req, res) => {
+app.get('/memo/:id', async (req, res) => {
     const { id } = req.params;
-    const product = await Product.findById(id)
-    res.render('products/show', { product })
+    const product = await Product.findById(id);
+    res.render('products/show', { product });
 });
 
-app.get('/products/:id/edit', async (req, res) => {
+app.get('/memo/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/edit', { product });
 });
 
-app.put('/products/:id', async (req, res) => {
+app.put('/memo/:id', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
-    res.redirect(`/products/${product._id}`);
+    res.redirect(`/memo/${product._id}`);
 });
 
-app.delete('/products/:id', async (req, res) => {
+app.delete('/memo/:id', async (req, res) => {
     const { id } = req.params;
     await Product.findByIdAndDelete(id);
-    res.redirect('/products');
+    res.redirect('/memo');
 });
+
+module.exports = app;
